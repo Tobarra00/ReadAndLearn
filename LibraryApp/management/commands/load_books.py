1ed537bed04a4a171010a35e2ee87460315e0673
Tobarra <pedro.tobarra.leal@gmail.com>
2024-01-05 14:21:32 +0100

    First commit

--- LibraryApp/management/commands/load_books.py
+++ LibraryApp/management/commands/load_books.py
@@ -8,33 +8,34 @@ class Command(BaseCommand):
     help = 'Loads new books from json file'
 
     def handle(self, *args, **options):
-    
-        path = os.path.join('..', '..', 'Read&Learn', 'books.json')
 
-        with open(path, "r") as books:
-            data = json.load(books)
-            
-            for book in data:
-                isbn=book['book']['ISBN']
-                existing_book = Book.objects.filter(isbn=isbn).first()
+        path = os.path.join('books.json')
+        try:
+            with open(path, "r") as books:
+                data = json.load(books)
+                
+                for book in data:
+                    isbn=book['book']['ISBN']
+                    existing_book = Book.objects.filter(isbn=isbn).first()
 
-                if existing_book is None:
-                    author = Author.objects.create(
-                        name=book['book']['author']['name'],
-                        other_books=book['book']['author']['otherBooks']
-                    )
-                    Book.objects.create(
-                        title=book['book']['title'],
-                        isbn=isbn,
-                        genre=book['book']['genre'],
-                        pages=book['book']['pages'],
-                        synopsis=book['book']['synopsis'],
-                        cover=book['book']['cover'],
-                        year=book['book']['year'],
-                        author=author
-                    )
-                    self.stdout.write(self.style.SUCCESS(f'Loaded book: {book["book"]["title"]}'))
-                else:
-                    self.stdout.write(self.style.NOTICE(f'Book already exists: {book["book"]["title"]}'))
-        
+                    if existing_book is None:
+                        author = Author.objects.create(
+                            name=book['book']['author']['name'],
+                            other_books=book['book']['author']['otherBooks']
+                        )
+                        Book.objects.create(
+                            title=book['book']['title'],
+                            isbn=isbn,
+                            genre=book['book']['genre'],
+                            pages=book['book']['pages'],
+                            synopsis=book['book']['synopsis'],
+                            cover=book['book']['cover'],
+                            year=book['book']['year'],
+                            author=author
+                        )
+                        self.stdout.write(self.style.SUCCESS(f'Loaded book: {book["book"]["title"]}'))
+                    else:
+                        self.stdout.write(self.style.NOTICE(f'Book already exists: {book["book"]["title"]}'))
+        except FileNotFoundError:
+            self.stdout.write(self.style.NOTICE("File not found. It may not be in the right directory. It should be at the same directory level as 'manage.py'"))
     
--- ReadAndLearn/settings.py
+++ ReadAndLearn/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Read&Learn',
-        'USER': 'postgres',
-        'PASSWORD': 'DW30KaA',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
